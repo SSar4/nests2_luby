1eab4ee6e544b867b8fb37f626b193be69576df0
Fulano de Tal <aras.strong8@gmail.com>
2021-11-17 08:43:25 -0300

    feature: autenticação e autorização

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -9,6 +9,7 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import { CredentialsDto } from '../auth/dto/credentials.dto';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -39,6 +40,17 @@ export class UserRepository extends Repository<User> {
           'Erro ao salvar o usuário no banco de dados',
         );
       }
+    }
+  }
+
+  async checkCredentials(credentialsDto: CredentialsDto): Promise<User> {
+    const { email, password } = credentialsDto;
+    const user = await this.findOne({ email, status: true });
+
+    if (user && (await user.checkPassword(password))) {
+      return user;
+    } else {
+      return null;
     }
   }
 
